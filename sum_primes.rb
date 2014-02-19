4a8dfe4a4cc0d1314360304b6efb045737ceb0f7
Courtland Caldwell <courtland.caldwell@cympel.com>
2014-02-19 13:00:46 -0600

    Renamed to sum_primes to ruby convention

---
+++ sum_primes.rb
@@ -0,0 +1,21 @@
+def get_primes num_needed
+  primes = [2]
+  current = 3
+  while primes.length < num_needed
+    divisor_found = false
+    primes.each {|prime| current % prime == 0 ? divisor_found = true : divisor_found = divisor_found }
+    unless divisor_found
+      primes << current
+    end
+    current += 2
+  end
+  return primes
+end
+
+def sum_array ar
+  sum = 0
+  ar.each {|prime| sum += prime}
+  return sum
+end
+
+puts sum_array (get_primes 1000)
